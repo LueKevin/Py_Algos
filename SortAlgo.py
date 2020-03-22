a3dfde1311f5f59b79b01cabdba15f9423c5bd53
unknown <kjl561@gmail.com>
2020-03-22 15:13:12 -0400

    second commit

--- SortAlgo.py
+++ SortAlgo.py
@@ -1,9 +1,11 @@
 #Sorting Algos
 
-class sAlgo():
+class sAlgo:
+    def __init__(self):
+
     def insSrt(list):
-        for i in range(0,list.length()):
-            j = i
-            while j and list[j-1] > list[j]:
-                list[j], list[j-1] = list[j-1], list[j]
+    for i in range(0,list.length()):
+        j = i
+        while j and list[j-1] > list[j]:
+            list[j], list[j-1] = list[j-1], list[j]
                 
